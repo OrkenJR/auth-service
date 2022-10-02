04505e3d4106647bb319129fd3df0a0eec85961c
Orken <orkenfreeman@gmail.com>
2022-10-02 20:06:25 +0600

    Добавил исключения в фильтр

--- src/main/java/kz/iitu/authservice/config/jwt/TokenFilter.java
+++ src/main/java/kz/iitu/authservice/config/jwt/TokenFilter.java
@@ -13,6 +13,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
 import static kz.iitu.authservice.util.StringUtil.isEmpty;
 
@@ -26,21 +27,26 @@ public class TokenFilter extends OncePerRequestFilter {
 
     private static final String AUTHORIZATION_HEADER = "Authorization";
 
+    private static final List<String> allowed = List.of("/login", "/register");
+
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         String token = request.getHeader(AUTHORIZATION_HEADER);
 
-        if (isEmpty(token)) {
-            throw new AuthException("ssKvz4iYss :: Please authorise into system to continue");
+        if (!allowed.contains(request.getRequestURI().substring(request.getContextPath().length()))) {
+
+            if (isEmpty(token)) {
+                throw new AuthException("ssKvz4iYss :: Please authorise into system to continue");
+            }
+            Token tokenDto = tokenRepository.findTokenById(token);
+
+            if (tokenDto == null) {
+                throw new AuthException("j3vSMqgT0f :: Your session is expired, please login again");
+            }
+
+            CurrentUserHolder.currentToken.set(tokenDto);
         }
-
-        Token tokenDto = tokenRepository.findTokenById(token);
-
-        if (tokenDto == null) {
-            throw new AuthException("j3vSMqgT0f :: Your session is expired, please login again");
-        }
-
-        CurrentUserHolder.currentToken.set(tokenDto);
 
         filterChain.doFilter(request, response);
     }
