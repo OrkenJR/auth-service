0b9284a6d8c481dd339bb69d2f1d0a150d9b2a18
Yerassyl <75775516+moooge89@users.noreply.github.com>
2022-10-09 22:00:30 +0600

    feat: use secured login

--- src/main/java/kz/iitu/authservice/controller/AuthController.java
+++ src/main/java/kz/iitu/authservice/controller/AuthController.java
@@ -3,6 +3,7 @@ package kz.iitu.authservice.controller;
 import kz.iitu.authservice.service.AuthService;
 import kz.iitu.cfaslib.controller.AbstractWrapper;
 import kz.iitu.cfaslib.dto.LoginRequest;
+import kz.iitu.cfaslib.dto.SecuredLoginRequest;
 import kz.iitu.cfaslib.dto.UserDto;
 import kz.iitu.cfaslib.feign.UserFeign;
 import lombok.RequiredArgsConstructor;
@@ -13,6 +14,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import static kz.iitu.cfaslib.util.SecureUtil.decode;
+
 @RestController
 @RequiredArgsConstructor
 public class AuthController extends AbstractWrapper {
@@ -21,7 +24,14 @@ public class AuthController extends AbstractWrapper {
     private final UserFeign userFeign;
 
     @PostMapping("/login")
-    public ResponseEntity<String> login(@RequestBody LoginRequest loginRequest) throws Exception {
+    public ResponseEntity<String> login(@RequestBody SecuredLoginRequest securedLoginRequest) throws Exception {
+        String username = decode(securedLoginRequest.username);
+        String password = decode(securedLoginRequest.password);
+
+        LoginRequest loginRequest = new LoginRequest();
+        loginRequest.setUsername(username);
+        loginRequest.setPassword(password);
+
         return responseWrap(authService.login(loginRequest));
     }
 
